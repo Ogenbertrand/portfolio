66f2c73f69c288f07c71fe8a531f7f84e1ab8a80
Ogenbertrand <ogenbertrand@gmail.com>
2024-11-13 17:43:13 +0100

    Add new designs

--- Portfolio/src/components/Contacts.tsx
+++ Portfolio/src/components/Contacts.tsx
@@ -1,15 +1,16 @@
 import '../assets/css/Contacts.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 const Contacts = () => {
     return (
         <div className="contacts">
-            <button className="get-in-touch-btn">Get in Touch</button>
+            <button className="get-in-touch-btn">Get in Touch <FontAwesomeIcon icon={faArrowRight} /></button>
             <button className="contact-btn">
-                Contact <span className="arrow-icon"></span>
+                Contact <FontAwesomeIcon icon={faArrowRight} />
             </button>
         </div>
     )
 }
 
 export default Contacts;
-
